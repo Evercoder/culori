0090c7f926864bce392007b9e49cdf0b1b8fcd43
Dan Burzo <danburzo@gmail.com>
2018-03-29 16:41:16 +0300

    sketching

--- src/color/color.js
+++ src/color/color.js
@@ -1,14 +1,32 @@
+import rgb_to_hsl from '../converters/rgb2hsl';
+import rgb_to_hsv from '../converters/rgb2hsv';
+
 class Color {
 
 	constructor(c) {
 		this._color = c;
 	}
 
-	to(format) {}
+	to(format) {
+		switch(format) {
+			case 'hsl':
+				return rgb_to_hsl(...this._color);
+			case 'hsv':
+				return rgb_to_hsv(...this._color);
+		}
+	}
 
 	nice(format) {}
 
-	css(format) {}
+	css(format) {
+		let nice = this.to(format);
+		switch(format) {
+			case 'hsl':
+				return `hsl(${nice[0]}, ${nice[1]}, ${nice[2]}, ${nice[3]})`;
+			case 'rgb':
+				return `rgb(${nice[0]}, ${nice[1]}, ${nice[2]}, ${nice[3]})`;
+		}
+	}
 
 	serialize() {
 		return { ...this._color };
