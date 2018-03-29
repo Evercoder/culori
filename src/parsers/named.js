41f9e466b5b0d5c5f7bff3312f1f534b72576683
Dan Burzo <danburzo@gmail.com>
2018-03-29 12:32:17 +0300

    Added transparent to named colors parser

--- src/parsers/named.js
+++ src/parsers/named.js
@@ -1,4 +1,8 @@
 import parseNumber from './number';
 import named_colors from '../colors/named';
 
-export default color => named_colors[color] ? parseNumber(named_colors[color]) : undefined;
+// Also supports the `transparent` color as defined in:
+// https://drafts.csswg.org/css-color/#transparent-black
+export default color => color === 'transparent' ? 
+	parseNumber(0x00000000) : 
+	named_colors[color] ? parseNumber(named_colors[color]) : undefined;
