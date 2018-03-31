2a16281c68fc540aa8e5632f7f034be6e913cfd5
Dan Burzo <danburzo@gmail.com>
2018-03-31 20:56:12 +0300

    Renamed d50 - d65

--- src/converters/lab_to_rgb.js
+++ src/converters/lab_to_rgb.js
@@ -1,9 +1,9 @@
 import lab_to_xyz from './lab_to_xyz';
-import d50_to_d65 from './d50_to_d65';
+import xyz_d50_to_d65 from './xyz_d50_to_d65';
 import xyz_to_rgb from './xyz_to_rgb';
 
 export default lab => {
-	let res = xyz_to_rgb(d50_to_d65(lab_to_xyz(lab)));
+	let res = xyz_to_rgb(xyz_d50_to_d65(lab_to_xyz(lab)));
 	res.mode = 'rgb';
 	if (lab.alpha !== undefined) res.alpha = lab.alpha;
 	return res;
