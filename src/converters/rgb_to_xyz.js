980ad3fcd2232b4eede87aa095adf1e1c6437000
Dan Burzo <danburzo@gmail.com>
2018-04-01 20:45:32 +0300

    fixes rgb to xyz

--- src/converters/rgb_to_xyz.js
+++ src/converters/rgb_to_xyz.js
@@ -9,8 +9,11 @@
 
 const rgb_to_lrgb = c => c < 0.04045 ? c / 12.92 : Math.pow((c + 0.055) / 1.055, 2.4);
 
-export default ({ r, g, b }) => ({
-	x: rgb_to_lrgb(0.4124564 * r + 0.3575761 * g + 0.1804375 * b),
-	y: rgb_to_lrgb(0.2126729 * r + 0.7151522 * g + 0.0721750 * b),
-	z: rgb_to_lrgb(0.0193339 * r + 0.1191920 * g + 0.9503041 * b)
-});
+export default ({ r, g, b }) => {
+	r = rgb_to_lrgb(r), g = rgb_to_lrgb(g), b = rgb_to_lrgb(b);
+	return {
+		x: 0.4124564 * r + 0.3575761 * g + 0.1804375 * b,
+		y: 0.2126729 * r + 0.7151522 * g + 0.0721750 * b,
+		z: 0.0193339 * r + 0.1191920 * g + 0.9503041 * b
+	};
+}
