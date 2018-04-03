10bffa5cda4b5cca18d54c2b2e5c2bbd3affd797
Dan Burzo <danburzo@gmail.com>
2018-04-03 16:18:09 +0300

    v0.2.12

--- src/api/swatches.js
+++ src/api/swatches.js
@@ -1,10 +1,11 @@
 export default (fn, count = 2) => {
 	if (count < 2) {
-		return undefined;
+		return count < 1 ? [fn(0), fn(1)] ? [fn(0.5)];
 	}
 	let res = [];
 	for (let i = 0; i < count; i++) {
 		res.push(fn(i / (count - 1)));
 	}
 	return res;
+	
 };
