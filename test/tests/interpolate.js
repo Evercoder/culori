fad7e0de1b88d3cd0992ab0dfb6a3f442aef60f5
Dan Burzo <danburzo@gmail.com>
2018-04-03 11:24:24 +0300

    comment out incomplete interpolate test

--- test/tests/interpolate.js
+++ test/tests/interpolate.js
@@ -2,9 +2,9 @@ let tape = require('tape');
 let culori = require('../../');
 let { interpolate } = culori;
 
-tape('caca', function(test) {
-	test.equal(interpolate([0, 1])(0.5), 0.5);
-	test.equal(interpolate([0, 1])(0.33), 0.33);
-	test.equal(interpolate([0, 100])(0.33), 33.33);
-	test.end();
-});
+// tape('caca', function(test) {
+// 	test.equal(interpolate([0, 1])(0.5), 0.5);
+// 	test.equal(interpolate([0, 1])(0.33), 0.33);
+// 	test.equal(interpolate([0, 100])(0.33), 33.33);
+// 	test.end();
+// });
