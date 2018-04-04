b8d8fc844beb91bf09e2108ab39e51ecb4a8faa6
Dan Burzo <danburzo@gmail.com>
2018-04-04 19:17:41 +0300

    Fixes achromatic interpolation in LCH space by rounding the chroma to 4 digits

--- src/converters/lab_to_lch.js
+++ src/converters/lab_to_lch.js
@@ -1,16 +1,20 @@
 import normalizeHue from '../util/normalizeHue';
+import round from '../api/round';
 /* 
 	References: 
 		* https://drafts.csswg.org/css-color/#lab-to-lch
 		* https://drafts.csswg.org/css-color/#color-conversion-code
 */
 export default ({ l, a, b, alpha }) => {
+	// Fixes achromatic colors having a _slight_ chroma due to floating-point errors
+	// and approximated computations in sRGB <-> CIELab.
+	let c = round(Math.sqrt(a * a + b * b), 4);
 	let res = {
 		mode: 'lch',
 		l: l,
-		c: Math.sqrt(a * a + b * b),
-		h: normalizeHue(Math.atan2(b, a) * 180 / Math.PI)
+		c: c
 	}
+	if (c) res.h = normalizeHue(Math.atan2(b, a) * 180 / Math.PI);
 	if (alpha !== undefined) res.alpha = alpha;
 	return res;
 };
--- src/converters/lch_to_lab.js
+++ src/converters/lch_to_lab.js
@@ -7,8 +7,8 @@ export default ({ l, c, h, alpha }) => {
 	let res = {
 		mode: 'lab',
 		l: l,
-		a: c * Math.cos( h / 180 * Math.PI ),
-		b: c * Math.sin( h / 180 * Math.PI )
+		a: c === 0 ? 0 : c * Math.cos( h / 180 * Math.PI ),
+		b: c === 0 ? 0 : c * Math.sin( h / 180 * Math.PI )
 	}
 	if (alpha !== undefined) res.alpha = alpha;
 	return res;
