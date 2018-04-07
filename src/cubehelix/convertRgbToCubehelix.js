19e0b4465aeb6588ccedbe6e473acc5631463d48
Dan Burzo <danburzo@gmail.com>
2018-04-08 01:47:36 +0300

    More cubehelix refactoring

--- src/cubehelix/convertRgbToCubehelix.js
+++ src/cubehelix/convertRgbToCubehelix.js
@@ -1,37 +1,37 @@
 /*
-	This RGB to Cubehelix conversion is based on 
-	Mike Bostock's work in d3-color.
+	Convert a RGB color to the Cubehelix HSL color space.
 
-	Reference: 
+	This computation is not present in Green's paper:
+	https://arxiv.org/pdf/1108.5083.pdf
+
+	...but can be derived from the inverse, HSL to RGB conversion.
+
+	It matches the math in Mike Bostock's D3 implementation:
+	
 	https://github.com/d3/d3-color/blob/master/src/cubehelix.js
  */
 
 import { radToDeg, M } from './constants';
 import normalizeHue from '../util/normalizeHue';
 
-let A = M[0],
-	B = M[1],
-	C = M[2],
-	D = M[3],
-	E = M[4];
-
-let ED = E * D,
-	EB = E * B,
-	BC_DA = B * C - D * A;
+let DE = M[3] * M[4];
+let BE = M[1] * M[4];
+let BC = M[1] * M[2];
+let AD = M[0] * M[3];
 
 export default ({ r, g, b, alpha }) => {
 
-	let l = (BC_DA * b + ED * r - EB * g) / (BC_DA + ED - EB);
-	let bl = b - l;
-	let k = (E * (g - l) - C * bl) / D;
+	let l = (r * DE - g * BE + (BC - AD) * b) / (BC - AD + DE - BE));
+	let y = b - l;
+	let x = ((g - l) * M[4] - M[2] * (b - l)) / M[3];
 
 	let res = { 
 		mode: 'cubehelix',
 		l: l,
-		s: (l === 0 || l === 1) ? undefined : Math.sqrt(k * k + bl * bl) / (E * l * (1 - l))
+		s: (l === 0 || l === 1) ? undefined : Math.sqrt(x * x + y * y) / (E * l * (1 - l))
 	};
 
-	if (res.s) res.h = Math.atan2(k, bl) * radToDeg - 120;
+	if (res.s) res.h = Math.atan2(y, x) * radToDeg - 120;
 	if (alpha !== undefined) res.alpha = alpha;
 
 	return res;
--- src/cubehelix/definition.js
+++ src/cubehelix/definition.js
@@ -7,6 +7,28 @@
 
 	https://www.mrao.cam.ac.uk/%7Edag/CUBEHELIX/
 	https://arxiv.org/pdf/1108.5083.pdf
+
+	Although Cubehelix was defined to be a method to obtain a colour scheme,
+	it actually contains a definition of a colour space, as identified by 
+	Mike Bostock and implemented in D3.js.
+
+	Green's paper introduces the following terminology:
+
+	* 	a `lightness` dimension in the interval [0, 1] 
+		on which we interpolate to obtain the colour scheme
+	*	a `start` colour that is analogous to a Hue in HSL space
+	*	a number of `rotations` around the Hue cyllinder.
+	*	a `hue` parameter which should more appropriately be called `saturation`
+	
+	As such, the original definition of the Cubehelix scheme is actually an
+	interpolation between two colors in the Cubehelix space:
+
+	H: start 				H: start + 360 * rotations
+	S: hue 			->		S: hue
+	L: 0					L: 1
+
+	We can therefore extend the interpolation to any two colors in this space,
+	with a variable Saturation and a Lightness interval other than the fixed 0 -> 1.
 */
 
 import { interpolateNumber, interpolateAlpha, interpolateHue } from '../interpolate';
