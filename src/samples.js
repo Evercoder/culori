d53083d2c734ee7edfedbddcd9ef05f6be62b182
Dan Burzo <danburzo@gmail.com>
2018-04-07 22:58:24 +0300

    remove gamma

--- src/samples.js
+++ src/samples.js
@@ -1,10 +1,10 @@
-export default (count = 2, gamma = 1) => {
+export default (count = 2) => {
 	if (count < 2) {
-		return count < 1 ? [] : [Math.pow(0.5, gamma)];
+		return count < 1 ? [] : [0.5];
 	}
 	let res = [];
 	for (let i = 0; i < count; i++) {
-		res.push(Math.pow(i / (count - 1), gamma));
+		res.push(i / (count - 1));
 	}
 	return res;
 };
