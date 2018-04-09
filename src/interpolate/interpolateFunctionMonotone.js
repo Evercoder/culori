5b8a5997ffc79a28c3a488951a67b82c432d5a47
Dan Burzo <danburzo@gmail.com>
2018-04-09 15:59:45 +0300

    adjustments?

--- src/interpolate/interpolateFunctionMonotone.js
+++ src/interpolate/interpolateFunctionMonotone.js
@@ -1,17 +1,17 @@
 const invariant = values => values;
 
-const monotone = (Vim1, Vi, Vip1, Vip2, h, t) => {
-	let h2 = h * h;
+const monotone = (Vim1, Vi, Vip1, Vip2, classes, t) => {
+	let classes2 = classes * classes;
 	let t2 = t * t;
 	let t3 = t2 * t;
 
-	let s1 = (Vip1 - Vim1) / h;
-	let s2 = (Vip2 - Vi) / h;
-	let s3 = (Vip1 - Vi) / h;
+	let s1 = (Vip1 - Vim1) * classes;
+	let s2 = (Vip2 - Vi) * classes;
+	let s3 = (Vip1 - Vi) * classes;
 
 	return (
-		(0.5 * s1 + 0.5 * s2 - 2 * s3) / h2 * t3 +
-		(3 * s3 - s1 - 0.5 * s2) / h * t2 + 
+		(0.5 * s1 + 0.5 * s2 - 2 * s3) * classes2 * t3 +
+		(3 * s3 - s1 - 0.5 * s2) * classes * t2 + 
 		0.5 * s1 * t +
 		Vi
 	);
@@ -24,8 +24,6 @@ export default (normalize = invariant, γ = 1) =>
 
 		let classes = arr.length - 1;
 		let i = t === 1 ? classes - 1 : Math.floor(t * classes);
-
-		let h = 1 / classes;
 		let Vi = arr[i];
 		let Vip1 = arr[i + 1];
 
@@ -35,5 +33,5 @@ export default (normalize = invariant, γ = 1) =>
 		let v = normalize([Vim1, Vi, Vip1, Vip2]);
 
 		return typeof v === 'object' ? 
-			monotone(v[0], v[1], v[2], v[3], h, (t - i / classes) * classes) : v;
+			monotone(v[0], v[1], v[2], v[3], classes, t * classes - Math.floor(t * classes)) : v;
 	};
