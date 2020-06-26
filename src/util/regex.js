9c3bc514d321537ca24bceb536d61d2df55c2aa7
Axel Rozo Brézillac <axel@rozobrezillac.pro>
2020-06-26 17:54:05 +0200

    Add percent parsing for Lab and Lch (#86)

--- src/util/regex.js
+++ src/util/regex.js
@@ -8,27 +8,39 @@
 
 const num = (/([+-]?\d*\.?\d+(?:[eE][+-]?\d+)?)/ + '').replace(/^\/|\/$/g, ''); // number
 const per = `${num}%`; // percentage
+const per_or_num = `${num}%?`; // percentage or number
 const alpha = `(?:${num}%|${num})`; // alpha-value
 const hue = `(?:${num}(deg|grad|rad|turn)|${num})`; // hue
 const c = `\\s*,\\s*`; // comma
 const s = `\\s+`; // space
 
-
 /*
 	rgb() regular expressions.
 	Reference: https://drafts.csswg.org/css-color/#rgb-functions
  */
-const rgb_num_old = new RegExp(`^rgba?\\(\\s*${num}${c}${num}${c}${num}\\s*(?:,\\s*${alpha}\\s*)?\\)$`);
-const rgb_per_old = new RegExp(`^rgba?\\(\\s*${per}${c}${per}${c}${per}\\s*(?:,\\s*${alpha}\\s*)?\\)$`);
-const rgb_num_new = new RegExp(`^rgba?\\(\\s*${num}${s}${num}${s}${num}\\s*(?:\\/\\s*${alpha}\\s*)?\\)$`);
-const rgb_per_new = new RegExp(`^rgba?\\(\\s*${per}${s}${per}${s}${per}\\s*(?:\\/\\s*${alpha}\\s*)?\\)$`);
+const rgb_num_old = new RegExp(
+	`^rgba?\\(\\s*${num}${c}${num}${c}${num}\\s*(?:,\\s*${alpha}\\s*)?\\)$`
+);
+const rgb_per_old = new RegExp(
+	`^rgba?\\(\\s*${per}${c}${per}${c}${per}\\s*(?:,\\s*${alpha}\\s*)?\\)$`
+);
+const rgb_num_new = new RegExp(
+	`^rgba?\\(\\s*${num}${s}${num}${s}${num}\\s*(?:\\/\\s*${alpha}\\s*)?\\)$`
+);
+const rgb_per_new = new RegExp(
+	`^rgba?\\(\\s*${per}${s}${per}${s}${per}\\s*(?:\\/\\s*${alpha}\\s*)?\\)$`
+);
 
 /*
 	hsl() regular expressions.
 	Reference: https://drafts.csswg.org/css-color/#the-hsl-notation
  */
-const hsl_old = new RegExp(`^hsla?\\(\\s*${hue}${c}${per}${c}${per}\\s*(?:,\\s*${alpha}\\s*)?\\)$`);
-const hsl_new = new RegExp(`^hsla?\\(\\s*${hue}${s}${per}${s}${per}\\s*(?:\\/\\s*${alpha}\\s*)?\\)$`);
+const hsl_old = new RegExp(
+	`^hsla?\\(\\s*${hue}${c}${per}${c}${per}\\s*(?:,\\s*${alpha}\\s*)?\\)$`
+);
+const hsl_new = new RegExp(
+	`^hsla?\\(\\s*${hue}${s}${per}${s}${per}\\s*(?:\\/\\s*${alpha}\\s*)?\\)$`
+);
 
 /*
 	hexadecimal regular expressions.
@@ -39,20 +51,28 @@ const hex = /^#?([0-9a-f]{8}|[0-9a-f]{6}|[0-9a-f]{4}|[0-9a-f]{3})$/i;
 	hwb() regular expressions.
 	Reference: https://drafts.csswg.org/css-color/#the-hwb-notation
  */
-const hwb = new RegExp(`^hwb\\(\\s*${hue}${s}${per}${s}${per}\\s*(?:\\/\\s*${alpha}\\s*)?\\)$`);
+const hwb = new RegExp(
+	`^hwb\\(\\s*${hue}${s}${per}${s}${per}\\s*(?:\\/\\s*${alpha}\\s*)?\\)$`
+);
 
 /*
 	lab() and lch() regular expressions.
 	Reference: https://drafts.csswg.org/css-color/#lab-colors
  */
-const lab = new RegExp(`^lab\\(\\s*${num}${s}${num}${s}${num}\\s*(?:\\/\\s*${alpha}\\s*)?\\)$`);
-const lch = new RegExp(`^lch\\(\\s*${num}${s}${num}${s}${hue}\\s*(?:\\/\\s*${alpha}\\s*)?\\)$`);
+const lab = new RegExp(
+	`^lab\\(\\s*${per_or_num}${s}${num}${s}${num}\\s*(?:\\/\\s*${alpha}\\s*)?\\)$`
+);
+const lch = new RegExp(
+	`^lch\\(\\s*${per_or_num}${s}${num}${s}${hue}\\s*(?:\\/\\s*${alpha}\\s*)?\\)$`
+);
 
 /*
 	gray() regular expressions.
 	Reference: https://drafts.csswg.org/css-color/#grays
  */
-const gray = new RegExp(`^gray\\(\\s*${num}()()\\s*(?:\\/\\s*${alpha}\\s*)?\\)$`);
+const gray = new RegExp(
+	`^gray\\(\\s*${num}()()\\s*(?:\\/\\s*${alpha}\\s*)?\\)$`
+);
 
 export {
 	rgb_num_old,
