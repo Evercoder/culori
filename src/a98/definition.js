1cd0e8b447767277e9dcbca017d423b1611bddda
Dan Burzo <dan@danburzo.ro>
2020-08-13 00:22:35 +0300

    Implements Adobe RGB (1998)

--- src/a98/definition.js
+++ src/a98/definition.js
@@ -1,7 +1,20 @@
 import rgb from '../rgb/definition';
 
+import convertA98ToXyz65 from './convertA98ToXyz65';
+import convertXyz65ToA98 from './convertXyz65ToA98';
+import convertRgbToXyz65 from '../xyz65/convertRgbToXyz65';
+import convertXyz65ToRgb from '../xyz65/convertXyz65ToRgb';
+
 export default {
 	...rgb,
 	mode: 'a98',
-	parsers: []
+	parsers: [],
+	input: {
+		rgb: color => convertXyz65ToA98(convertRgbToXyz65(color)),
+		xyz65: convertXyz65ToA98
+	},
+	output: {
+		rgb: color => convertXyz65ToRgb(convertA98ToXyz65(color)),
+		xyz65: convertA98ToXyz65
+	}
 };
--- test/xyz65.test.js
+++ test/xyz65.test.js
@@ -2,12 +2,17 @@ import tape from 'tape';
 import { xyz65 } from '../src/index';
 
 tape('xyz65', t => {
+	/*
+		These should theoretically be equal to the Xn, Yn, and Zn values
+		from xyz65/constants.js, but there's a small rounding error for `y`.
+	 */
 	t.deepEqual(xyz65('white'), {
 		mode: 'xyz65',
 		x: 0.95047,
 		y: 1.0000001,
 		z: 1.08883
 	});
+
 	t.deepEqual(xyz65('black'), { mode: 'xyz65', x: 0, y: 0, z: 0 });
 	t.deepEqual(xyz65('red'), {
 		mode: 'xyz65',
