d3be69020653b2a984257a79331abfaf1c80b5e9
Bijela Gora <bijela_gora@protonmail.com>
2022-04-10 16:39:29 +0200

    Improve readability

--- src/random.d.ts
+++ src/random.d.ts
@@ -4,9 +4,11 @@ type NumberOrRange = number | [number, number];
 
 type Find<C, M extends Mode> = C extends { mode: M } ? C : never;
 
-type Constraints<M extends Mode> = {
-	[P in keyof Partial<Omit<Find<Color, M>, 'mode'>>]: NumberOrRange;
-};
+type TakeColorChannels<M extends Mode> = Omit<Find<Color, M>, 'mode'>;
+
+type Constraints<M extends Mode> = Partial<{
+	[P in keyof TakeColorChannels<M>]: NumberOrRange;
+}>;
 
 export default function random<M extends Mode>(
 	mode: M,
