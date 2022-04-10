4295553508c212c4dedace562a5300af26184ad9
Bijela Gora <bijela_gora@protonmail.com>
2022-04-10 22:14:56 +0200

    Add some types

---
+++ src/hsl/convertHslToRgb.d.ts
@@ -0,0 +1,4 @@
+import type { Hsl } from './types';
+import type { RgbWithMode } from '../rgb/types';
+
+export default function convertHslToRgb(color: Hsl): RgbWithMode;
---
+++ src/hsl/convertRgbToHsl.d.ts
@@ -0,0 +1,4 @@
+import type { Rgb } from '../rgb/types';
+import type { HslWithMode } from './types';
+
+export default function convertRgbToHsl(color: Rgb): HslWithMode;
