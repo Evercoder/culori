41f36c3952e958bb70d5eb5827392c661362b017
Bijela Gora <bijela_gora@protonmail.com>
2022-04-11 18:56:07 +0200

    Update types

--- src/average.d.ts
+++ src/average.d.ts
@@ -1,21 +1,19 @@
-import type { Color, Find, Mode, TakeColorChannels } from './common';
+import type {
+	Color,
+	Find,
+	Mode,
+	OverridesFunction,
+	OverridesObject
+} from './common';
 
 declare function averageAngle(val: number[]): number;
 
 declare function averageNumber(val: Array<number>): number;
 
-type OverrideFn = (values: number[], channel: string) => number;
-
-type Overrides<M extends Mode> =
-	| OverrideFn
-	| Partial<{
-			[P in keyof TakeColorChannels<M>]: OverrideFn;
-	  }>;
-
 declare function average<M extends Mode>(
 	colors: (Color | string)[],
 	mode: M,
-	overrides?: Overrides<M>
+	overrides?: OverridesFunction | OverridesObject<M>
 ): Find<Color, M>;
 
 export { average, averageAngle, averageNumber };
--- src/common.d.ts
+++ src/common.d.ts
@@ -3,10 +3,24 @@ import { HslWithMode } from './hsl/types';
 import { LabWithMode } from './lab/types';
 import type { LrgbWithMode } from './lrgb/types';
 import type { Xyz50WithMode } from './xyz50/types';
+import type { A98WithMode } from './a98/types';
+import type { CubehelixWithMode } from './cubehelix/types';
+import type { DlabWithMode } from './dlab/types';
+import type { DlchWithMode } from './dlch/types';
+import type { HsiWithMode } from './hsi/types';
+import type { HsvWithMode } from './hsv/types';
+import type { HwbWithMode } from './hwb/types';
 
 export type Color =
-	| RgbWithMode
+	| A98WithMode
+	| CubehelixWithMode
+	| DlabWithMode
+	| DlchWithMode
+	| HsiWithMode
 	| HslWithMode
+	| HsvWithMode
+	| HwbWithMode
+	| RgbWithMode
 	| LabWithMode
 	| LrgbWithMode
 	| Xyz50WithMode;
@@ -16,3 +30,9 @@ export type Mode = Pick<Color, 'mode'>['mode'];
 export type Find<C, M extends Mode> = C extends { mode: M } ? C : never;
 
 export type TakeColorChannels<M extends Mode> = Omit<Find<Color, M>, 'mode'>;
+
+export type OverridesFunction = (values: number[], channel: string) => number;
+
+export type OverridesObject<M extends Mode> = Partial<{
+	[P in keyof TakeColorChannels<M>]: OverridesFunction;
+}>;
