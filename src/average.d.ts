99a274d577a9a2ddf81b7715914a65f6149ae946
Bijela Gora <bijela_gora@protonmail.com>
2022-04-14 19:40:38 +0200

    Improve types for 'average' function

--- src/average.d.ts
+++ src/average.d.ts
@@ -2,16 +2,24 @@ import type {
 	Color,
 	Find,
 	Mode,
+	NonEmptyArray,
 	OverridesFunction,
 	OverridesObject
 } from './common';
+import type { Rgb } from './rgb/types';
 
 declare function averageAngle(val: number[]): number;
 
 declare function averageNumber(val: number[]): number;
 
+declare function average(colors: NonEmptyArray<Color | string>): Rgb;
+declare function average(
+	colors: NonEmptyArray<Color | string>,
+	mode: undefined,
+	overrides?: OverridesFunction | OverridesObject<'rgb'>
+): Rgb;
 declare function average<M extends Mode>(
-	colors: (Color | string)[],
+	colors: NonEmptyArray<Color | string>,
 	mode: M,
 	overrides?: OverridesFunction | OverridesObject<M>
 ): Find<Color, M>;
