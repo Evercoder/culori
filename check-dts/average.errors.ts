80475d36fcd66ad340f7293e9156f270aaa2566c
Bijela Gora <bijela_gora@protonmail.com>
2022-04-16 00:59:22 +0200

    Second file moved from snapshot testing to check-dts

---
+++ check-dts/average.errors.ts
@@ -0,0 +1,25 @@
+import { average } from '../src/average';
+import type { Rgb } from '../src/rgb/types';
+import type { Lch } from '../src/lch/types';
+import type { Oklab } from '../src/oklab/types';
+
+// THROWS Expected 1-3 arguments, but got 0.
+const c1 = average();
+
+// THROWS Argument of type '[]' is not assignable to parameter of type 'NonEmptyArray<string | Color>'.
+const c2 = average([]);
+
+// THROWS No overload matches this call.
+const c3: Rgb = average(['#ff0000', '#0000ff'], undefined, {
+	r: 1
+});
+
+// THROWS No overload matches this call.
+const c4: Rgb = average(['#ff0000', '#0000ff'], 'hsl', {
+	r: () => 1
+});
+
+// THROWS Type 'Hsl' is missing the following properties from type 'Rgb': r, g, b
+const c5: Rgb = average(['#ff0000', '#0000ff'], 'hsl', {
+	h: () => 1
+});
