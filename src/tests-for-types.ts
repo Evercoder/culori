a53d67ca2b9d6dbe671f9b46ba1dc0473ca37fa5
Bijela Gora <bijela_gora@protonmail.com>
2022-04-15 23:55:08 +0200

    Remove file in which I checked type definitions

--- src/tests-for-types.ts
+++
@@ -1,7 +0,0 @@
-import type { Rgb } from './rgb/types';
-import type { Color } from './common';
-import parseTransparent from './rgb/parseTransparent';
-import prepare from './_prepare';
-
-const p1: undefined = parseTransparent('asd');
-const p2: Rgb = parseTransparent('transparent');
