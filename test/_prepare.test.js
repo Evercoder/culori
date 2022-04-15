0b5135a954c2ddeb06ce35f82aaed15f0f45a18f
Bijela Gora <bijela_gora@protonmail.com>
2022-04-15 23:52:52 +0200

    Add tests for prepare function

---
+++ test/_prepare.test.js
@@ -0,0 +1,35 @@
+import tape from 'tape';
+import { a98, formatCss } from '../src/index.js';
+import prepare from '../src/_prepare.js';
+
+tape('prepare', t => {
+	t.equal(prepare(undefined), undefined);
+
+	t.equal(
+		prepare({
+			r: 0.2549019607843137,
+			g: 0.4117647058823529,
+			b: 0.8823529411764706
+		}),
+		undefined
+	);
+
+	t.deepEqual(
+		prepare(
+			{
+				r: 0.2549019607843137,
+				g: 0.4117647058823529,
+				b: 0.8823529411764706
+			},
+			'rgb'
+		),
+		{
+			mode: 'rgb',
+			r: 0.2549019607843137,
+			g: 0.4117647058823529,
+			b: 0.8823529411764706
+		}
+	);
+
+	t.end();
+});
