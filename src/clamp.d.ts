c77099a6a62d62e3275b604cabff59a8e57e8f2b
Bijela Gora <bijela_gora@protonmail.com>
2022-04-15 23:53:18 +0200

    Simplify types for clamp functions

--- src/clamp.d.ts
+++ src/clamp.d.ts
@@ -1,13 +1,12 @@
-import type { Color, Find, Mode } from './common';
+import type { Color, Mode } from './common';
 
-export declare function clampRgb(color: string): Color;
-export declare function clampRgb<C extends Find<Color, M>, M extends Mode>(
-	color: C
-): C;
+export declare function clampRgb(color: string): Color | undefined;
+export declare function clampRgb<C extends Color>(color: C): C;
 
-export declare function clampChroma(color: string): Color;
+export declare function clampChroma(color: string): Color | undefined;
 export declare function clampChroma<C extends Color>(color: C): C;
-export declare function clampChroma<
-	C extends Find<Color, M>,
-	M extends Mode = 'lch'
->(color: C, mode: M): C;
+export declare function clampChroma<C extends Color>(color: C, mode: Mode): C;
+export declare function clampChroma<C extends Color>(
+	color: string,
+	mode: Mode
+): Color | undefined;
--- src/tests-for-types.ts
+++ src/tests-for-types.ts
@@ -1,52 +1,7 @@
-import { clampChroma, clampRgb } from './clamp';
 import type { Rgb } from './rgb/types';
 import type { Color } from './common';
-import type { Oklch } from './oklch/types';
 import parseTransparent from './rgb/parseTransparent';
 import prepare from './_prepare';
 
-const f: Color = clampRgb('rgba(255, 0, 0, 0.5)');
-
-const g: Rgb = clampRgb({
-	mode: 'rgb',
-	r: 0.2549019607843137,
-	g: 0.4117647058823529,
-	b: 0.8823529411764706
-});
-
-const h: Color = clampChroma('rgb(300, 255, 255)');
-const k: Oklch = clampChroma({
-	mode: 'oklch',
-	l: 0.5,
-	c: 0.161,
-	h: 180
-});
-const l: Oklch = clampChroma(
-	{ mode: 'oklch', l: 0.5, c: 0.16, h: 180 },
-	'oklch'
-);
-
 const p1: undefined = parseTransparent('asd');
 const p2: Rgb = parseTransparent('transparent');
-
-const pr: undefined = prepare(undefined);
-const pr2: Color | undefined = prepare('red');
-const pr3: Rgb = prepare({
-	mode: 'rgb',
-	r: 0.2549019607843137,
-	g: 0.4117647058823529,
-	b: 0.8823529411764706
-});
-const pr4: Rgb = prepare(
-	{
-		r: 0.2549019607843137,
-		g: 0.4117647058823529,
-		b: 0.8823529411764706
-	},
-	'rgb'
-);
-const pr5: undefined = prepare({
-	r: 0.2549019607843137,
-	g: 0.4117647058823529,
-	b: 0.8823529411764706
-});
