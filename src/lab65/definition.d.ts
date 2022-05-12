f2d5f77fe99523b4d1b9d4bfe3eca79d6e7a3e8f
Bijela Gora <bijela_gora@protonmail.com>
2022-05-12 20:37:51 +0200

    Add one more definition.d.ts

---
+++ src/lab65/definition.d.ts
@@ -0,0 +1,30 @@
+import convertLab65ToRgb from './convertLab65ToRgb.js';
+import convertLab65ToXyz65 from './convertLab65ToXyz65.js';
+import convertRgbToLab65 from './convertRgbToLab65.js';
+import convertXyz65ToLab65 from './convertXyz65ToLab65.js';
+import lab from '../lab/definition.js';
+
+declare const definition: typeof lab & {
+	mode: 'lab65';
+
+	parse: ['--lab-d65'];
+	serialize: '--lab-d65';
+
+	toMode: {
+		xyz65: typeof convertLab65ToXyz65;
+		rgb: typeof convertLab65ToRgb;
+	};
+
+	fromMode: {
+		xyz65: typeof convertXyz65ToLab65;
+		rgb: typeof convertRgbToLab65;
+	};
+
+	ranges: {
+		l: [0, 100];
+		a: [-86.182, 98.234];
+		b: [-107.86, 94.477];
+	};
+};
+
+export default definition;
