53804f1c0f6ed4ea1dbf3e706fa30fcea456ce31
Bijela Gora <bijela_gora@protonmail.com>
2022-05-13 10:10:30 +0200

    More accurate type def for converter factory

--- src/converter.d.ts
+++ src/converter.d.ts
@@ -1,10 +1,11 @@
 import { Color, FindColorByMode, Mode } from './common';
 
-export type ConvertFn<M extends Mode> = (
-	color: Color | string
-) => FindColorByMode<M> | undefined;
+export interface ConvertFn<M extends Mode = 'rgb'> {
+	(color: Color, target_mode?: M): FindColorByMode<M>;
+	(color: string, target_mode?: M): FindColorByMode<M> | undefined;
+}
 
-declare function converter(): ConvertFn<'rgb'>;
+declare function converter(): ConvertFn;
 declare function converter<M extends Mode>(target_mode: M): ConvertFn<M>;
 
 export default converter;
