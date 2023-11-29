3091a6b23e96f867115e122ec87dd367269c39d9
Dan Burzo <dan@danburzo.ro>
2023-11-29 16:43:57 +0200

    Add test to confirm averageAngle() changes

--- test/average.test.js
+++ test/average.test.js
@@ -1,8 +1,13 @@
 import tape from 'tape';
-import { average, formatHex } from '../src/index.js';
+import { average, averageAngle, formatHex } from '../src/index.js';
 
 tape('average', t => {
 	t.equal(formatHex(average(['#ff0000', '#0000ff'])), '#800080');
 	t.equal(formatHex(average(['#ff0000', '#0000ff'], 'lch')), '#f50086');
+	t.end();
+});
+
+tape('averageAngle', t => {
+	t.equal(averageAngle([270, 0]), 315);
 	t.end();
 });
