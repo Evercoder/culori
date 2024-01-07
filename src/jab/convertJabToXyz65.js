e4ea8b38b3395221b05a51a9affc8f9b44fdc1e9
Dan Burzo <dan@danburzo.ro>
2024-01-07 22:39:48 +0200

    Adds PQ EOTF/EOTF^-1 (interal for now), clarify Jzazbz transfer function re: PQ

--- src/jab/convertJabToXyz65.js
+++ src/jab/convertJabToXyz65.js
@@ -1,14 +1,16 @@
-const n = 0.1593017578125; // = 2610 / Math.pow(2, 14);
+import { M1 as n, C1, C2, C3 } from '../hdr/transfer.js';
 const p = 134.03437499999998; // = 1.7 * 2523 / Math.pow(2, 5);
-const c1 = 0.8359375; // = 3424 / Math.pow(2, 12);
-const c2 = 18.8515625; // = 2413 / Math.pow(2, 7);
-const c3 = 18.6875; // = 2392 / Math.pow(2, 7);
 const d0 = 1.6295499532821566e-11;
 
-/* `v` may be negative, in which case return 0 instead of NaN */
-const pq_inv = v => {
+const npow = (v, exp) => Math.sign(v) * Math.pow(Math.abs(v), exp);
+
+/* 
+	The encoding function is derived from Perceptual Quantizer.
+*/
+const jabPqDecode = v => {
+	if (v < 0) return 0;
 	let vp = Math.pow(v, 1 / p);
-	return 10000 * Math.pow((c1 - vp) / (c3 * vp - c2), 1 / n) || 0;
+	return 10000 * Math.pow((C1 - vp) / (C3 * vp - C2), 1 / n);
 };
 
 const rel = v => v / 203;
@@ -16,9 +18,9 @@ const rel = v => v / 203;
 const convertJabToXyz65 = ({ j, a, b, alpha }) => {
 	let i = (j + d0) / (0.44 + 0.56 * (j + d0));
 
-	let l = pq_inv(i + 0.13860504 * a + 0.058047316 * b);
-	let m = pq_inv(i - 0.13860504 * a - 0.058047316 * b);
-	let s = pq_inv(i - 0.096019242 * a - 0.8118919 * b);
+	let l = jabPqDecode(i + 0.13860504 * a + 0.058047316 * b);
+	let m = jabPqDecode(i - 0.13860504 * a - 0.058047316 * b);
+	let s = jabPqDecode(i - 0.096019242 * a - 0.8118919 * b);
 
 	let res = {
 		mode: 'xyz65',
